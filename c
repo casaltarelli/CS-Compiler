d6486a2942eb5809c55ebd8f77244939315d1655
Casaltarelli <casaltarelli@gmail.com>
2021-02-26 18:46:13 -0500

    Added npm + TypeScript Dependencies

---
+++ c
@@ -0,0 +1,2 @@
+#!/bin/sh
+tsc --rootDir source/ --outDir distrib/  source/*.ts
---
+++ source/global.ts
@@ -0,0 +1,15 @@
+/* ------------
+   Globals.ts
+
+   Global CONSTANTS and _Variables.
+   (Global over both the OS and Hardware Simulation / Host.)
+
+   This code references page numbers in our text book:
+   Operating System Concepts 8th edition by Silberschatz, Galvin, and Gagne.  ISBN 978-0-470-12872-5
+   ------------ */
+
+//
+// Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
+//
+const APP_NAME: string    = "CSCompiler";
+const APP_VERSION: string = "0.1";
