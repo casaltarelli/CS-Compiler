12678f76ae4cb41692adb925de26f35a202b0371
Casaltarelli <casaltarelli@gmail.com>
2021-02-26 18:52:17 -0500

    Testing TypeScript Compiler

---
+++ distrib/global.js
@@ -0,0 +1,14 @@
+/* ------------
+   Globals.ts
+
+   Global CONSTANTS and _Variables.
+   (Global over both the OS and Hardware Simulation / Host.)
+
+   This code references page numbers in our text book:
+   Operating System Concepts 8th edition by Silberschatz, Galvin, and Gagne.  ISBN 978-0-470-12872-5
+   ------------ */
+//
+// Global CONSTANTS (TypeScript 1.5 introduced const. Very cool.)
+//
+var APP_NAME = "CSCompiler";
+var APP_VERSION = "0.1";
