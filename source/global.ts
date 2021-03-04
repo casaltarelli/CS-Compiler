b8b1edd5219c88d912818872ca7dc698c6162f06
Casaltarelli <casaltarelli@gmail.com>
2021-03-04 17:24:55 -0500

    Developed UI

--- source/global.ts
+++ source/global.ts
@@ -2,7 +2,6 @@
    Globals.ts
 
    Global CONSTANTS and _Variables.
-   (Global over both the OS and Hardware Simulation / Host.)
 
    This code references page numbers in our text book:
    Operating System Concepts 8th edition by Silberschatz, Galvin, and Gagne.  ISBN 978-0-470-12872-5
